aea944a8765a49354fbe195a683bf60d0d60d6fa
master <mathieugreen13@gmail.com>
2022-11-04 14:07:06 -0500

    Initial commit

---
+++ src/homeworkarrays.java
@@ -0,0 +1,34 @@
+import java.io.Console;
+import java.util.*;
+import java.util.Scanner;
+
+
+public class homeworkarrays {
+    public static void main(String[] args) {
+
+        double[ ] exampleArray = {1,5,6,5,4,1};
+
+        double maximum = examplesArray[0];
+
+        int index = 0;
+
+        for (int i = 1; i<exampleArray.length>; i++){
+
+            if (exampleArray[ i ] > maximum) {
+
+                maximum = exampleArray[ i ];
+
+                index = i;
+
+            }
+
+        }
+
+        System.out.println(index);
+
+        //it gives illegal start of expression
+        //it gives class, interface, enum or record expected
+
+        }
+    }
+}
